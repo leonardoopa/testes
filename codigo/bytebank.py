09ac04e749c19817d05d52a8e779c26dc98f712d
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2025-06-15 21:25:27 -0300

    feat: TDD test

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -16,11 +16,26 @@ class Funcionario:
         return self._salario
 
     def idade(self):
-        partes_data = self._data_nascimento.split('/')
-        ano_nascimento_str = partes_data[2]  
+        partes_data = self._data_nascimento.split("/")
+        ano_nascimento_str = partes_data[2]
         ano_atual = date.today().year
-        
+
         return ano_atual - int(ano_nascimento_str)
+
+    def sobrenome(self):
+        nome_completo = self.nome.strip()
+        nome_quebrado = nome_completo.split(" ")
+        return nome_quebrado[-1]
+    
+    def _verificar_socio(self):
+        sobrenomes = ["Gabriel", "Leandro", "Davi"]
+        return self.salario >= 100000 and (self.sobrenome() in sobrenomes)
+
+    def decrescimo_salario(self):
+        if self._verificar_socio():
+            decrescimo = self._salario *0.1
+            self._salario = self._salario - decrescimo
+        return self._salario
 
     def calcular_bonus(self):
         valor = self._salario * 0.1
@@ -30,4 +45,3 @@ class Funcionario:
 
     def __str__(self):
         return f"Funcionario({self._nome}, {self._data_nascimento}, {self._salario})"
- 
--- codigo/main.py
+++ codigo/main.py
@@ -1,4 +1,4 @@
-from bytebank import Funcionario # Linha alterada
+from bytebank import Funcionario  # Linha alterada
 
 # funcionario = Funcionario("João", "1990/01/01", 3000)
 # print(funcionario.nome)
@@ -6,10 +6,9 @@ from bytebank import Funcionario # Linha alterada
 # print(funcionario.calcular_bonus())
 
 
-
 def test_idade():
     funcionario_test = Funcionario("Leonardo", "01/01/2003", 5000)
-    print(f'Idade: {funcionario_test.idade()}')
+    print(f"Idade: {funcionario_test.idade()}")
 
 
-test_idade()    
+test_idade()
