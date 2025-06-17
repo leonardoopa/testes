fbc018dd45bf1e96f5827783ffe1519c448bf636
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2025-06-17 17:35:07 -0300

    feat

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,4 +1,6 @@
 from codigo.bytebank import Funcionario  # Linha alterada
+import pytest
+from pytest import mark
 
 
 class TestClass:
@@ -20,7 +22,7 @@ class TestClass:
         resultado = funcionario_test.sobrenome()  # When ação
 
         assert resultado == esperado  # Then desfecho
-
+    
     def test_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
         entrada_salario = 100000
         entrada_nome = "Gabriel"
@@ -30,3 +32,24 @@ class TestClass:
         resultado = funcionario_test.decrescimo_salario()
 
         assert resultado == esperado
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000    # Given esperado
+        esperado = 100
+
+        funcionario_test = Funcionario("Gabriel", "01/01/2003", entrada)
+        resultado = funcionario_test.calcular_bonus() # When ação
+
+        assert resultado == esperado # Then desfecho
+
+
+    def test_quando_calcular_bonus_recebe_10000000_deve_retornar_expt(self):
+        with pytest.raises(Exception):
+            # Corrigido: Use o valor de entrada que causa o erro.
+            entrada = 10000000
+
+            funcionario_test = Funcionario("Gabriel", "01/01/2003", entrada)
+        
+            funcionario_test.calcular_bonus()
+
